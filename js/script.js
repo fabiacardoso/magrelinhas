c210cf76ebce4d8d986f54a2eb28603db36bb262
Fábia Emanuele Cardoso Sena <61851602+fabiacardoso@users.noreply.github.com>
2021-02-11 22:58:16 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-new SimpleSlide({
-  slide: 'quoat', // nome do atributo data-slide="principal"
-  time: 5000, // tempo de transição dos slides
-});
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
+
+	new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
+
+}
